9c537f02f6e2102801e11228aa28a5e13b6b5265
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2024-11-25 21:16:08 +0800

    更新软件信息

--- src/about.rs
+++ src/about.rs
@@ -7,8 +7,16 @@ pub fn show_about_window(ctx: &egui::Context, open: &mut bool) {
         .collapsible(false)
         .show(ctx, |ui| {
             ui.heading("AppData Cleaner");
-            ui.label("作者: TC999");
-            ui.label("源代码仓库: https://github.com/TC999/AppDataCleaner");
+            ui.horizontal(|ui| {
+                ui.label("作者: ");
+                // 将作者名字包装为可点击链接
+                ui.hyperlink_to("TC999", "https://github.com/TC999"); // 点击名字跳转到指定链接
+            });
+            // 添加可点击的链接
+            ui.horizontal(|ui| {
+                ui.label("源代码仓库:");
+                ui.hyperlink("https://github.com/TC999/AppDataCleaner");
+            });
             ui.label("许可证: GPL-3.0");
             ui.label("版本: 1.0.0");
         });
